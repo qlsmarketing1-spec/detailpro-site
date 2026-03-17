19021ab65cd9119c93aaea07504653fe9edb425a
qlsmarketing1-spec <qlsmarketing1@gmail.com>
2026-03-17 15:27:51 -0600

    Fix robots.txt sitemap URL to www

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://detailpro.tech/sitemap.xml',
+    sitemap: 'https://www.detailpro.tech/sitemap.xml',
   };
 }
